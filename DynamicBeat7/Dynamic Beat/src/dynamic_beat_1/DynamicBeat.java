934b0bb2a7316dde32dfb08b8cd08f0e66d32554
kangsh9107 <68472349+kangsh9107@users.noreply.github.com>
2020-08-17 19:51:09 +0900

    DynamicBeat7

---
+++ DynamicBeat7/Dynamic Beat/src/dynamic_beat_1/DynamicBeat.java
@@ -0,0 +1,21 @@
+package dynamic_beat_1;
+
+import javax.swing.JFrame;
+
+public class DynamicBeat extends JFrame {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public DynamicBeat() {
+		setTitle("Dynamic Beat");
+		setSize(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+
+}
